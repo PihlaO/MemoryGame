28d9d3df3626c2faded98368db8975a078940efd
Oksanen Pihla M <okpiok@da1-cs-b221-20.helsinki.fi>
2015-01-27 13:44:46 +0200

    päivitys

---
+++ MemoryGame/src/main/java/memorygame/memorygame/Kayttoliittyma.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memorygame.memorygame;
+
+import java.awt.Container;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author okpiok
+ */
+public class Kayttoliittyma implements Runnable {
+
+    private JFrame frame;
+
+    public Kayttoliittyma() {
+
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void luoKomponentit(Container container) {
+    }
+
+}
--- MemoryGame/src/test/java/memorygame/memorygame/KorttipakkaTest.java
+++ MemoryGame/src/test/java/memorygame/memorygame/KorttipakkaTest.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package memorygame.memorygame;
 
 import org.junit.After;
@@ -18,29 +17,51 @@ import static org.junit.Assert.*;
  * @author okpiok
  */
 public class KorttipakkaTest {
-    
+
+    Korttipakka korttipakkaHelppo;
+    Korttipakka korttipakkaKeskitaso;
+    Korttipakka korttipakkaVaikea;
+
     public KorttipakkaTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
+        korttipakkaHelppo = new Korttipakka(1);
+        korttipakkaKeskitaso = new Korttipakka(2);
+        korttipakkaVaikea = new Korttipakka(3);
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void luodaankoHelppoPakkaOikein() {
+        korttipakkaHelppo.luoKortit();
+        assertEquals(12, korttipakkaHelppo.vaikeustaso.haeKorttienLukumaara());
+
+    }
+
+    @Test
+    public void luodaankoKeskitasonPakkaOikein() {
+        korttipakkaKeskitaso.luoKortit();
+        assertEquals(16, korttipakkaKeskitaso.vaikeustaso.haeKorttienLukumaara());
+
+    }
+
+    @Test
+    public void luodaankoVaikeaPakkaOikein() {
+        korttipakkaVaikea.luoKortit();
+        assertEquals(20, korttipakkaVaikea.vaikeustaso.haeKorttienLukumaara());
+
+    }
 }
